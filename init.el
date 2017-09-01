f7269c476f36f14575e13d4388c6cc9075a76452
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-08-31 22:01:57 -0400

    Initial commit

---
+++ init.el
@@ -0,0 +1,12 @@
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+
