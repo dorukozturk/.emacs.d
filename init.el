4ee2563fb0ef63f6d2b550f6bdab1d0102fca479
Doruk Ozturk <doruk.ozturk@kitware.com>
2017-09-01 08:58:45 -0400

    Custom boilerplate

--- init.el
+++ init.el
@@ -53,3 +53,18 @@
 (use-package zenburn-theme
   :ensure t
   :init (load-theme 'zenburn t))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (which-key f ag helm-ag helm-projectile helm zenburn-theme projectile ivy magit use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
